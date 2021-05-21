1725fb4691f7bc3ba42b8d74efff1ec97e140293
Nikolius Lau <n1colius.lau@gmail.com>
2021-05-21 17:02:59 +0700

    update icons

--- frontend/service.js
+++ frontend/service.js
@@ -50,7 +50,8 @@ self.addEventListener("push", function(event) {
 
 const showLocalNotification = (title, body, swRegistration) => {
   const options = {
-    body
+    "body": body,
+    "icon": "images.png"
     // here you can add more properties like icon, image, vibrate, etc.
   };
   swRegistration.showNotification(title, options);
